766bbf575a366fc20e4e7106623e51f243661e3e
goudekid <goudekid@hotmail.com>
2017-11-21 21:34:35 -0500

    V1.1 - Added colorblind support

---
+++ ShowMeWhere/ShowMeWhere/MapInterface_Patch.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using RimWorld;
+using RimWorld.Planet;
+using System;
+using Verse;
+
+namespace ShowMeWhere
+{
+    [HarmonyPatch(typeof(MapInterface), "MapInterfaceUpdate", null)]
+    public static class MapInterface_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            if (Find.VisibleMap == null || WorldRendererUtility.WorldRenderedNow)
+            {
+                return;
+            }
+            Main.Instance.UpdateMap();
+        }
+    }
+}
